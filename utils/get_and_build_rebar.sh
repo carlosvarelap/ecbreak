b853d0459d147d059c2c7701204e6e6be0c6d52f
Carlos Varela <carlos.varela.paz@gmail.com>
2011-04-18 21:50:44 +0200

    FUNCT added a script to rebuild rebar

--- utils/get_and_build_rebar.sh
+++ utils/get_and_build_rebar.sh
@@ -1,2 +1,8 @@
-wget http://hg.basho.com/rebar/downloads/rebar
-chmod u+x rebar
+#!/bin/sh
+mkdir -p ext_libs
+cd ext_libs
+git clone git://github.com/basho/rebar.git
+cd rebar
+git checkout ta-qc
+PATH=$OTP_DIR/bin:$PATH ./bootstrap
+cp -i rebar $ECBREAK_DIR
