f4918a8b02b2922189d7f4f3b8d72f1e59e2fb31
Carlos Varela <carlos.varela.paz@gmail.com>
2011-04-19 16:39:26 +0200

    TEST improved open cicuit calls QC test

--- test/prop_ecbreak.erl
+++ test/prop_ecbreak.erl
@@ -30,20 +30,17 @@ ok_call() ->
 call() ->
     eqc_gen:oneof([bad_call(), fail_call(), ok_call()]).
 
-list_call() ->
+call_list() ->
     eqc_gen:list([bad_call(), fail_call(), ok_call()]).
 
 
 %% generates and dep list of calls that finished qith the circuit
 %% opened. The trick is to generate incorrect calls or one good call
 %% followed by two bad calls
-list_call_to_open_circuit(Threshold) ->
-    lists:map(
-      fun(_) ->
-              eqc_gen:oneof(
-                [incorrect_call(),
-                 [ok_call(), incorrect_call(), incorrect_call()]])
-      end, lists:seq(0,Threshold)).
+call_list_to_open_circuit(Threshold) ->
+    ql_gen:list_of_size(
+      Threshold*2,
+      call()).
 
 threshold() ->
     ?SUCHTHAT(X, eqc_gen:nat() , X>0).
@@ -106,13 +103,13 @@ prop_open_circuit_calls() ->
     ?FORALL(
        Threshold, threshold(),
        ?FORALL(
-          List, list_call_to_open_circuit(Threshold),
+          List, call_list_to_open_circuit(Threshold),
           begin
               application:start(ecbreak),
               ecbreak:set_failure_threshold(Threshold),
               ecbreak:reset(),
               generate_module(),
-              {Opened, ProcesedElements} =
+              {Fail, ProcesedElements} =
                   lists:foldl(
                     fun(_, {true, Acc}) ->
                          {true, Acc};
@@ -120,25 +117,49 @@ prop_open_circuit_calls() ->
                         {false, Acc}) ->
                             try
                                 ecbreak:call(M,F,A),
-                                case Acc of
-                                    0 ->
-                                        {false, Acc};
-                                    _ ->
-                                        {false, Acc-1}
-                                end
+                                {false, Acc+1}
                             catch
                                 open_circuit ->
                                     {true, Acc+1};
                                 _Else ->
                                     {false, Acc+1}
                             end
-                    end, {false, 0}, lists:flatten(List)),
+                    end, {false, 0}, List),
               application:stop(ecbreak),
               delete_module(),
               ?WHENFAIL(
                  io:format("Expected ~p, got ~p~n",
-                           [{true, Threshold+1},
-                            {Opened, ProcesedElements}]),
-                 {Opened, ProcesedElements} =:= {true, Threshold+1}
+                           [should_fail(List, Threshold),
+                            {Fail, ProcesedElements}]),
+                 collect(with_title("Circuit opened"),
+                         element(1,should_fail(List, Threshold)),
+                   {Fail, ProcesedElements} =:= should_fail(List, Threshold)
+                  )
                 )
           end)).
+
+%% @returns false | {true, NumberOfPProcesedElements}
+should_fail(List, Threshold) ->
+    should_fail(List,  0, Threshold, Threshold).
+
+should_fail(
+  [{_,_} | _L], ProcesedElements,
+  _Threshold, Remainder) when Remainder =:= 0->
+    {true, ProcesedElements+1};
+should_fail(
+  [{_,{throw, _}} | L], ProcesedElements,
+  Threshold, Remainder) ->
+    should_fail(L, ProcesedElements+1, Threshold, Remainder-1);
+should_fail(
+  [{_,_}|L], ProcesedElements, Threshold, Remainder)
+  when Threshold =:= Remainder ->
+    should_fail(L, ProcesedElements+1, Threshold, Remainder);
+should_fail(
+  [{_,_}|L], ProcesedElements, Threshold, Remainder) ->
+    should_fail(L, ProcesedElements+1, Threshold, Remainder+1);
+should_fail(
+  [], ProcesedElements, _Threshold, _Remainder) ->
+    {false, ProcesedElements}.
+
+
+
